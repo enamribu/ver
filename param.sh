988a0434b02d91c0a0c0f19c75f7a435c117ab7f
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2025-06-09 12:52:28 +0700

    Update param.sh

--- param.sh
+++ param.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mapfile -t cmds < param.txt
+mapfile -t cmds < keyhunt_commands.txt
 
 for i in "${!cmds[@]}"; do
     idx=$((i+1))
     screen -dmS screen$idx bash -c "
         cd keyhunt
-        echo | script -q -c 'python3 BTC.py' /dev/null > btcpy-log$idx.txt 2>&1 &
-        stdbuf -oL -eL ${cmds[i]} | tee keyhunt-log$idx.txt
+        echo | script -q -c 'python3 BTC.py' /dev/null &
+        stdbuf -oL -eL ${cmds[i]}
     "
 done
